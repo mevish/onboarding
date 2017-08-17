e24ee7e42df4271a79c820a54a63b64b901d785d
Vladimir Melnikov <mevish@5.255.232.153-red.dhcp.yndx.net>
2017-08-17 16:29:40 +0300

    Show input value in alert

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, {Component} from 'react';
+import { FlatButton, TextField } from 'material-ui';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+
+let value;
+
+function onClick() {
+    alert(value);
+}
+
+function saveInputValue(e, data) {
+    value = data;
+}
+
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to Direct</h2>
+            <MuiThemeProvider>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to Direct</h2>
+                    </div>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <TextField onChange={saveInputValue} />
+                    <FlatButton label="SUPER BUTTON" onClick={onClick} />
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
